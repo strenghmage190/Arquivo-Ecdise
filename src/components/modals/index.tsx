b42fa15a4a7274ee41a9510d871240ae329008e1
strenghmage190 <strenghmage28@gmail.com>
2026-01-02 19:47:29 -0300

    feat: export refactored modals and investigation tabs in barrel export

--- src/components/modals/index.tsx
+++ src/components/modals/index.tsx
@@ -1,6 +1,11 @@
 export { default as CreateClueModal } from './CreateClueModal';
 export { default as InvestigationCardModal } from './InvestigationCardModal';
+export { default as CreateClueModal_Refactored } from './CreateClueModal_Refactored';
+export { default as InvestigationCardModal_Refactored } from './InvestigationCardModal_Refactored';
 export { default as CodePromptModal } from './CodePromptModal';
 
 // Sistema centralizado: tudo em CreateClueModal
 export { default as CreatorHub } from './CreatorHub';
+
+// Barrel export for tabs
+export * from './investigationTabs';
